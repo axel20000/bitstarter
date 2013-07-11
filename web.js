d120592e1d682fe01f33ed2fa991bd723b178234
Axel <axel20000@gmail.com>
2013-07-11 17:40:37 -0500

    Actualizados web.js y index.html
    Definida estructura html/css

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
+/// codigo propio
+var fs= require('fs');
+var salida="";
+
+salida= fs.readFileSync('index.html').toString();
+
+
+////Original de web.js salvo salida
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 4!');
+  response.send(salida);
 });
 
 var port = process.env.PORT || 5000;
